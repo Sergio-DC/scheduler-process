b3e74de813ee2d551581eb3c0322db635899d7da
Sergio <a01365942@itesm.mx>
2018-09-01 17:49:12 -0500

    Version 1.4 alterna: se modifico la funcion readFile() ya que la anterior no podia leer numeros de mas de 1 digito, siendo esta mas eficiente

--- scheduler.c
+++ scheduler.c
@@ -19,16 +19,18 @@ struct PCB
 struct PCB *first = NULL, *last = NULL, *actual = NULL, *new = NULL;
 int quantum;
 int contador = 0;// Está variable determinará el tamaño de la lista enlazada
+int num1,num2,num3,num4;
 
 void readFile();//función que nos permite leer un archivo de texto y tokenizar los datos
 void releaseMemory();//función que se utiliza para liberar memoria cuando el hayamos finalizado el programa
 void genLinkedList(int, int, int, int);//Genera una lista enlazada
-void showLinkedList();// muestra la lista enlazada con datos importantes del planificador
+void showLinkedList();//Funcion Debug() muestra la lista enlazada con datos importantes del planificador
+void showOrderedList(); //Funcion Debug() que muestra la lista enlazada ordenada
 int completionTime(struct PCB *, int);//calcula el tiempo en el que finalizo un proceso después de ejecutarse
 void turnAroundTime(struct PCB *);//calcula el tiempo de retorno por cada procese
 void waitingTime(struct PCB *);// calcula el tiempo de espera de cada proceso
-void sortById();
-void showOrderedList(); //Funcion de prueba
+void sortById();//Ordena la lista enlazada de menor a mayor
+
 
 int main()
 {
@@ -50,47 +52,27 @@ void readFile()
       char * token;
       int digit;
 
-      archivo = fopen("input2.txt", "r");
+      archivo = fopen("input.txt", "r"); //Abrimos el archivo txt que se encuntra en la misma ubicación que scheduler.c
 
-
-      while (fgets(num,20, archivo))
+      while (!feof(archivo))
       {
-
-            if(contador == 0)
-            {
-                printf("quantum: %s\n", num);
-                quantum = (int)num - 48;
-                contador++;
-            }
-            else
-            {
-                printf("original: %s\n", num);
-
-                token = strtok(num," ");
-                digit = (int)*token - 48;
-                procces_id = digit;
-
-                token = strtok(NULL," ");
-                digit = (int) *token - 48;
-                arrival_time = digit;
-
-                token = strtok(NULL," ");
-                digit = (int) *token - 48;
-                cpu_burst = digit;
-
-                token = strtok(NULL," ");
-                digit = (int) *token - 48;
-                priority = digit;
-
-                printf("contador: %d\n\n", contador++);
-                //Creamos una lista ENLAZADA
-                genLinkedList(procces_id, arrival_time, cpu_burst, priority);
-            }
-
+          if(contador == 0)
+          {
+              fscanf(archivo,"%d", &quantum);
+              printf("quantum: %d\n", quantum);
+              contador++;
+          }
+          else
+          {
+              //leemos los 4 valores del txt y los guardmos
+              int x = fscanf(archivo,"%d %d %d %d\n",&procces_id, &arrival_time,&cpu_burst,&priority);
+              //Guardamos los datos en un bloque de memoria que formara parte de una Lista enlazada
+              genLinkedList(procces_id, arrival_time, cpu_burst, priority);
+              contador++;
+          }
       }
 
         fclose(archivo);
-
 }
 
 void releaseMemory()
@@ -100,10 +82,8 @@ void releaseMemory()
 
     while (actual != NULL)
     {
-
         aux_free = actual;
         actual = actual->next;
-
         free(aux_free);
     }
 
@@ -116,7 +96,6 @@ void releaseMemory()
 
 void genLinkedList(int procces_id, int arrival_time, int cpu_burst, int priority)
 {
-
       //1. Reservar Espacio en memoria
       new = (struct PCB *)malloc(sizeof(struct PCB));
       new->procces_id = procces_id;
@@ -184,7 +163,7 @@ void waitingTime(struct PCB *process)
     process->waitingTime = process->turnAroundTime - process->cpu_burst;
 }
 
-void sortById()//Deberia ser para burstTime y priority
+void sortById()//Ordena la lista enlazada en función de cpu_burst
 {
   int i, hop=1;
   struct PCB *aux = NULL, *anterior = NULL;
@@ -192,10 +171,9 @@ void sortById()//Deberia ser para burstTime y priority
 
     for(i=0; i<contador-1; i++)
     {
-        printf("Entre exterior\n");
         actual = first;
         while (actual->next != NULL)
-        {   printf("Entre\n");
+        {
             aux = actual->next;
             if(actual->cpu_burst > aux->cpu_burst && hop==1)
             {
@@ -204,7 +182,6 @@ void sortById()//Deberia ser para burstTime y priority
                 anterior = aux;
                 first = aux;
                 hop++;
-                printf("Entre if 1\n");
             }
             else if(actual->cpu_burst > aux-> cpu_burst)
             {
@@ -212,7 +189,6 @@ void sortById()//Deberia ser para burstTime y priority
                 aux->next = actual;
                 anterior->next = aux;
                 anterior = aux;
-                printf("Entre if 2\n");
             }
             else
             {
@@ -227,7 +203,6 @@ void sortById()//Deberia ser para burstTime y priority
                 {
                     anterior = actual;
                     actual = actual->next;
-                    printf("Entre if 3\n");
                 }
             }
         }
